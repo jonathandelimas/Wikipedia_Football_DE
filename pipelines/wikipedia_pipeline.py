0a7ec586b137bfcdc732d78d1c6ca2b0e582bd25
jonathandelimas <johnlimma@gmail.com>
2024-01-17 21:52:46 +1100

    extrction finished and working

--- pipelines/wikipedia_pipeline.py
+++ pipelines/wikipedia_pipeline.py
@@ -1,3 +1,4 @@
+import json
 def get_wikipedia_page(url):
     import requests
 
@@ -15,16 +16,51 @@ def get_wikipedia_page(url):
 def get_wikipedia_data(html):
     from bs4 import BeautifulSoup
 
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, "html.parser")
     table = soup.find_all("table", {"class": "wikitable sortable"})[0]
 
-    table_rows = table.find_all('tr')
+    table_rows = table.find_all("tr")
 
     return table_rows
 
+def clean_text(text):
+    text = str(text).strip()
+    text = text.replace('&nbsp', '')
+    if text.find(' ♦'):
+        text = text.split(' ♦')[0]
+    if text.find('[') != -1:
+        text = text.split('[')[0]
+    if text.find(' (formerly)') != -1:
+        text = text.split(' (formerly)')[0]
+
+    return text.replace('\n', '')
+
 def extract_wikipedia_data(**kwargs):
+
     url = kwargs["url"]
     html = get_wikipedia_page(url)
     rows = get_wikipedia_data(html)
+
+    data = []
+
+    for i in range(1, len(rows)):
+        tds = rows[i].find_all("td")
+        values = {
+            'rank': i,
+            'stadium': clean_text(tds[0].text),
+            'capacity': clean_text(tds[1].text).replace(',', '').replace('.', ''),
+            'region': clean_text(tds[2].text),
+            'country': clean_text(tds[3].text),
+            'city': clean_text(tds[4].text),
+            'images': 'https://' + tds[5].find('img').get('src').split("//")[1] if tds[5].find('img') else "NO_IMAGE",
+            'home_team': clean_text(tds[6].text),
+        }
+        data.append(values)
+
+    json_rows = json.dumps(data)
+    kwargs["ti"].xcom_push(key="rows", value=json_rows)
+    return "Ok"
+
+
+def transform_wikipedia_data():
     
-    print(f"Extracting data from rows {rows}")
